9c83fe164b3a2f170bc6e9fccec71d5e2f8a26f0
Yushan Zheng <yszheng@buaa.edu.cn>
2022-12-04 16:22:12 +0800

    Update configure_dataset.py

--- dataset/configure_dataset.py
+++ dataset/configure_dataset.py
@@ -10,7 +10,7 @@ import os
 '''
 The script to configure the dataset for running the code.
 Take these TCGA Slides as the example.
-You may need to these Slides to the experimental files. 
+You may need to convert these slides to the experimental files. 
 Please refer to https://github.com/zhengyushan/tcga2tile.
 '''
 
